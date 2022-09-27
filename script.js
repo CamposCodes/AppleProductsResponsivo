4a075bb662af17a099630b7b2ff19ab246b67daf
Gabriel Campos Lima Alves <campos.gabriel@estudante.ufjf.br>
2022-09-27 13:28:24 -0300

    adiciona eventos de troca de personagem no Js

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
 let avatarName  = document.querySelector(".avatar-nome");
-let avatarImg   = document.querySelector(".avatar-img");
+let quant = document.querySelectorAll(".avatar-card .avatar-img");
+let atual = 0;
+let avatarAtual = document.getElementById("atual");
 let setaE       = document.querySelector("#seta-esquerda");
 let setaD       = document.querySelector("#seta-direita");
 
 
-setaE.addEventListener("click", () => {
+function animationName(){
     if(avatarName.classList.contains('animation-avatar')){
         avatarName.classList.remove('animation-avatar');
     }
     else{
         avatarName.classList.add('animation-avatar');
     }
+}
+
+function slide(){
+    if(atual >= quant.length){
+        atual = 0;
+    }
+    else if(atual < 0){
+        atual = quant.length - 1;
+    }
+    avatarAtual.style.marginLeft =  -20 * atual  + 'rem';
+}
+
+
+
+setaE.addEventListener("click", () => {
+    animationName();
+    atual --;
+    slide();
 });
 setaD.addEventListener("click", () => {
-    if(avatarName.classList.contains('animation-avatar')){
-        avatarName.classList.remove('animation-avatar');
-    }
-    else{
-        avatarName.classList.add('animation-avatar');
-    }
+    animationName();
+    atual++;
+    slide();
 });
+
