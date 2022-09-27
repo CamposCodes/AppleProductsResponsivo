67494a3e2bf238eaaa89aa1409d7d19134ba65f5
Gabriel Campos Lima Alves <campos.gabriel@estudante.ufjf.br>
2022-09-27 11:48:28 -0300

    atualiza js

--- script.js
+++ script.js
@@ -5,22 +5,18 @@ let setaD       = document.querySelector("#seta-direita");
 
 
 setaE.addEventListener("click", () => {
-    if(avatarName.classList.contains('animation-avatar') && avatarImg.classList.contains('animation-avatar')){
+    if(avatarName.classList.contains('animation-avatar')){
         avatarName.classList.remove('animation-avatar');
-        avatarImg.classList.remove('animation-avatar');
     }
     else{
         avatarName.classList.add('animation-avatar');
-        avatarImg.classList.add('animation-avatar');
     }
 });
 setaD.addEventListener("click", () => {
-    if(avatarName.classList.contains('animation-avatar') && avatarImg.classList.contains('animation-avatar')){
+    if(avatarName.classList.contains('animation-avatar')){
         avatarName.classList.remove('animation-avatar');
-        avatarImg.classList.remove('animation-avatar');
     }
     else{
         avatarName.classList.add('animation-avatar');
-        avatarImg.classList.add('animation-avatar');
     }
 });
