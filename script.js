d1178fda52b6b09fd87a162d5ca70a9b99935919
Gabriel Campos Lima Alves <campos.gabriel@estudante.ufjf.br>
2022-09-28 23:22:55 -0300

    transforma a pagina em um mostruario de produtos da apple ultilizando o mesmo conceito da anterior

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
 let quant = document.querySelectorAll(".avatar-card .avatar-img");
 let atual = 0;
-let avatarAtual = document.getElementById("atual");
+let productAtual = document.getElementById("atual");
 let setaE       = document.querySelector("#seta-esquerda");
 let setaD       = document.querySelector("#seta-direita");
 let nome = 0;
-let nomeAtual = document.getElementById("nome-atual");
 
-function slideName(){
-    if(nome >= quant.length){
-        nome = 0;
-    }
-    else if(nome < 0){
-        nome = quant.length - 1;
-    }
-    nomeAtual.style.marginLeft =  -17 * nome  + 'rem';
-}
 
 function slide(){
     if(atual >= quant.length){
@@ -23,21 +13,17 @@ function slide(){
     else if(atual < 0){
         atual = quant.length - 1;
     }
-    avatarAtual.style.marginLeft =  -20 * atual  + 'rem';
+    productAtual.style.marginLeft =  -20 * atual  + 'rem';
 }
 
 
 
 setaE.addEventListener("click", () => {
     atual --;
-    nome --;
     slide();
-    slideName();
 });
 setaD.addEventListener("click", () => {
     atual++;
-    nome ++;
     slide();
-    slideName();
 });
 
